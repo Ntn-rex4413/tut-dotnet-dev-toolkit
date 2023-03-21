9494a21f948c56551bd6c10cbc294b7f5ff77ea9
Natan Jodkowski <nj054413@student.ath.edu.pl>
2023-03-21 21:00:53 +0100

    Implement custom middleware and example showing services' lifetimes signified by their displayed ids both from endpoint and middleware

--- dependency-injection/DiApi/Program.cs
+++ dependency-injection/DiApi/Program.cs
@@ -1,5 +1,7 @@
 using DiApi.Data;
 using DiApi.DataServices;
+using DiApi.Middleware;
+using DiApi.Utility;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,14 +9,20 @@ builder.Services.AddSingleton<IDataRepo, SqlDataRepo>();
 
 builder.Services.AddScoped<IDataService, HttpDataService>();
 
+builder.Services.AddTransient<IOperationTransient, Operation>();
+builder.Services.AddScoped<IOperationScoped, Operation>();
+builder.Services.AddSingleton<IOperationSingleton, Operation>();
+
 var app = builder.Build();
+
+app.UseCustomMiddleware();
 
 app.UseHttpsRedirection();
 
-app.MapGet("/getdata", (IDataRepo repo) =>
+app.MapGet("/getdata", (IOperationTransient transient, IOperationScoped scoped, IOperationSingleton singleton) =>
 {
 
-    repo.ReturnData();
+    Console.WriteLine($"Endpoint: TransientId: {transient.OperationId}, ScopedId: {scoped.OperationId}, SingletonId: {singleton.OperationId}");
 
     return Results.Ok();
 });
---
+++ dependency-injection/DiApi/Utility/IOperation.cs
@@ -0,0 +1,13 @@
+﻿namespace DiApi.Utility
+{
+    public interface IOperation
+    {
+        string OperationId { get; }
+    }
+
+    public interface IOperationTransient : IOperation { }
+
+    public interface IOperationScoped : IOperation { }
+
+    public interface IOperationSingleton : IOperation { }
+}
