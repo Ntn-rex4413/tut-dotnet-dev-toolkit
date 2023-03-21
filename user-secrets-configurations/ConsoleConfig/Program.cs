c2f22a93e752b1bfff55e99102a63bdc5a43c4ec
Natan Jodkowski <nj054413@student.ath.edu.pl>
2023-03-22 00:27:39 +0100

    Add package in .csproj and set up basic config

--- user-secrets-configurations/ConsoleConfig/Program.cs
+++ user-secrets-configurations/ConsoleConfig/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+
+// specify configuration sources and order of them
+IConfigurationBuilder builder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddUserSecrets(typeof(Program).Assembly, optional: true);
+
+IConfigurationRoot config = builder.Build();
+
+Console.WriteLine("I think the password is: " + config["Password"]);
