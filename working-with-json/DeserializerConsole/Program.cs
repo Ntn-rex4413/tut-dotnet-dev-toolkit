cf3e69601c56ee5778285c32fb63991a19602b22
Natan Jodkowski <nj054413@student.ath.edu.pl>
2023-03-22 23:26:44 +0100

    Simple deserialize with options

--- working-with-json/DeserializerConsole/Program.cs
+++ working-with-json/DeserializerConsole/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using DeserializerConsole.Models;
+
+string fileName = "person.json";
+
+string jsonString = File.ReadAllText(fileName);
+
+var opt = new JsonSerializerOptions
+{
+    PropertyNameCaseInsensitive = true
+};
+
+Person? person = JsonSerializer.Deserialize<Person>(jsonString, opt);
+
+// ! is not a best practice, normally check if it isn't null
+Console.WriteLine($"The first name is: {person!.FirstName}");
