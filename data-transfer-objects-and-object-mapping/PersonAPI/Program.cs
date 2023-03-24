752c4926a2f347762addc143f533a13db0876adb
Natan Jodkowski <nj054413@student.ath.edu.pl>
2023-03-24 23:48:12 +0100

    Create base for API with EF and docker, with manual object mapping before DTO implementation

---
+++ data-transfer-objects-and-object-mapping/PersonAPI/Program.cs
@@ -0,0 +1,86 @@
+using Microsoft.EntityFrameworkCore;
+using PersonAPI.Data;
+using PersonAPI.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opt => 
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("SQLDbConnection")));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("api/v1/people", async (AppDbContext context) =>
+{
+    var people = await context.People.ToListAsync();
+
+    return Results.Ok(people);
+});
+
+app.MapGet("api/v1/people/{id}", async (AppDbContext context, int id) =>
+{
+    var person = await context.People.FindAsync(id);
+
+    if (person == null)
+    {
+        return Results.NotFound();
+    }
+
+    return Results.Ok(person);
+});
+
+app.MapPost("api/v1/people", async (AppDbContext context, Person person) =>
+{
+    await context.People.AddAsync(person);
+
+    await context.SaveChangesAsync();
+
+    return Results.Created($"/api/v1/people/{person.Id}", person);
+});
+
+app.MapPut("api/v1/people/{id}", async (AppDbContext context, int id, Person person) =>
+{
+    var personModel = await context.People.FindAsync(id);
+
+    if (person == null)
+    {
+        return Results.NotFound();
+    }
+
+    personModel.FullName = person.FullName;
+    personModel.Telephone = person.Telephone;
+    personModel.DoB = person.DoB;
+
+    await context.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("api/v1/people/{id}", async (AppDbContext context, int id) =>
+{
+    var person = await context.People.FindAsync(id);
+
+    if (person == null)
+    {
+        return Results.NotFound();
+    }
+
+    context.People.Remove(person);
+
+    await context.SaveChangesAsync();
+
+    return Results.Ok(person);
+});
+
+app.Run();
+
