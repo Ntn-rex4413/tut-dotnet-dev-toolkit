2e637c62908fd9bd5d1ec81c64be94550814f018
Natan Jodkowski <nj054413@student.ath.edu.pl>
2023-03-28 18:53:23 +0200

    Add SQL connection using user secrest and SqlConnectionStringBuilder

--- minimal-api/MinAPI/Program.cs
+++ minimal-api/MinAPI/Program.cs
@@ -1,7 +1,20 @@
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using MinApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var sqlConnectionBuilder = new SqlConnectionStringBuilder();
+
+sqlConnectionBuilder.ConnectionString = builder.Configuration.GetConnectionString("SQLDbConnection");
+// creation of user secrets in "user-secrets-creation.PNG" in the same commit
+sqlConnectionBuilder.UserID = builder.Configuration["UserId"];
+sqlConnectionBuilder.Password = builder.Configuration["Password"];
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(sqlConnectionBuilder.ConnectionString));
 
 var app = builder.Build();
 
